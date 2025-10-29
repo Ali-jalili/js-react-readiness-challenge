9dbe1121791b27343360fd1e74997b38bc3605e8
Ali Jalili <mrali97jl@gmail.com>
2025-10-29 16:01:38 +0330

    `Added initial VDOM implementation in tests/app.js`

---
+++ Day-02-VDOM-Diff/tests/app.js
@@ -0,0 +1,13 @@
+import { h } from "../core/h.js";
+
+
+// 1. ساختار ساده: یک تگ h1 با متن
+const VNode1 = h('h1', { id: 'title' }, 'Hello VDOM!');
+
+// 2. ساختار تودرتو: یک div با دو فرزند
+const VNode2 = h('div', { class: 'container' }, [
+    VNode1, // فرزند اول: VNode1
+    h('p', null, 'This is a child paragraph.') // فرزند دوم: یک VNode دیگر
+]);
+
+
