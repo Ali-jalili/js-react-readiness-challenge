838abe60605320ccb67989c30fb11b7c96e3168e
Ali Jalili <mrali97jl@gmail.com>
2025-10-29 18:20:26 +0330

    `Added diff function implementation with null checks and type comparison`

--- Day-02-VDOM-Diff/core/diff.js
+++ Day-02-VDOM-Diff/core/diff.js
@@ -1,2 +1,32 @@
+import { mount } from "./mount.js"
 
 
+export const diff = function (oldVNode, newVNode, parentEl) {
+
+    if (newVNode === null) {
+
+        if (oldVNode) {
+            parentEl.removeChild(oldVNode.el)
+        }
+
+        return
+    }
+
+    if (oldVNode === null) {
+        mount(newVNode, parentEl);
+        return;
+    }
+
+    if (oldVNode.type !== newVNode.type) {
+
+        parentEl.removeChild(oldVNode.el)
+
+        mount(newVNode, parentEl)
+
+        return
+    }
+
+
+
+
+}
