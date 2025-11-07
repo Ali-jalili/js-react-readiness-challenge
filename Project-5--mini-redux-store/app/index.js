09706c56b02c0abd88fa508a5b10b44d48119889
Ali Jalili <mrali97jl@gmail.com>
2025-11-07 15:48:27 +0330

    `Added initial DOM element references in app/index.js`

--- Project-5--mini-redux-store/app/index.js
+++ Project-5--mini-redux-store/app/index.js
@@ -1,1 +1,7 @@
 
+// app/index.js - مرحله ۱: گرفتن المان‌های DOM
+
+const counterValue = document.getElementById('counter-value');
+const incrementBtn = document.getElementById('increment-btn');
+const decrementBtn = document.getElementById('decrement-btn');
+
