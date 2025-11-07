56bd05aa838a8cae32a3efe1e6463765c618fba3
Ali Jalili <mrali97jl@gmail.com>
2025-11-07 23:45:52 +0330

    Adds memoize utility for function caching
    Introduces a memoize function to cache results of expensive function calls. This prevents redundant computations for the same inputs, improving performance.
    
    The utility stores results in an in-memory cache, using `JSON.stringify` to generate unique keys from function arguments. Includes console logging for cache hits and misses to aid debugging.

--- project-6--functional-toolbox/utils/index.js
+++ project-6--functional-toolbox/utils/index.js
@@ -18,3 +18,35 @@ export const debounce = function (func, delay) {
 
     }
 }
+
+
+export const memoize = function (func) {
+
+    let cache = {};
+
+
+    return function (...args) {
+
+        const key = JSON.stringify(args);
+
+        if (key in cache) {
+
+            console.log(`Cache Hit! برگرداندن نتیجه ذخیره‌شده برای: ${key}`);
+
+            return cache[key]; // ... نتیجه را مستقیماً برگردان
+        }
+
+        const result = func.apply(this, args);
+
+        // ذخیره نتیجه جدید در Cache قبل از برگرداندن
+        cache[key] = result;
+
+        console.log(`Cache Miss! محاسبه جدید انجام و ذخیره شد.`);
+
+        return result;
+
+    }
+
+
+
+}
