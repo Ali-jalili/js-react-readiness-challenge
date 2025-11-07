6d604a119ff15f3254a0bcc48c675c3a95d8875b
Ali Jalili <mrali97jl@gmail.com>
2025-11-07 16:26:51 +0330

    `Removed 56 lines of code from createStore function`

--- Project-5--mini-redux-store/core/createStore.js
+++ Project-5--mini-redux-store/core/createStore.js
@@ -1,57 +1,5 @@
 //? هسته مدیریت state 
 
-// export const createStore = function (reducer, preloadedState) {
-
-//     let state = preloadedState;
-//     const listeners = [];
-
-
-
-//     let obj = {
-
-//         getState: function () {
-//             return state
-//         },
-
-//         dispatch: function (action) {
-//             state = reducer(state, action);
-
-//             listeners.forEach(fun => fun())
-//         },
-
-//         subscribe: function (listener) {
-
-//             listeners.push(listener);
-
-//             const unsubscribe = function () {
-
-//                 const itemIndex = listeners.indexOf(listener)
-
-//                 listeners.splice(itemIndex, 1)
-
-
-//             }
-
-//             return unsubscribe
-
-//         },
-
-
-
-//     }
-
-
-//     dispatch({ type: '@@INIT' });
-
-//     return {
-//         getState,
-//         dispatch,
-//         subscribe
-//     }
-
-// }
-
-
 export const createStore = function (reducer, preloadedState) {
 
     let state = preloadedState;
