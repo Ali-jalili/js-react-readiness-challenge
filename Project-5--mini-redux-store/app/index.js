69dc409a7d7150977c5d22c7c755c2ef885b014b
Ali Jalili <mrali97jl@gmail.com>
2025-11-07 15:59:33 +0330

    `Added createStore, counterReducer, and action imports to app/index.js`

--- Project-5--mini-redux-store/app/index.js
+++ Project-5--mini-redux-store/app/index.js
@@ -1,3 +1,7 @@
+import { createStore } from "../core/createStore.js";
+import { counterReducer } from '../app/reducer.js';
+import { incrementAction, decrementAction } from "./actions.js";
+
 
 // app/index.js - مرحله ۱: گرفتن المان‌های DOM
 
@@ -5,3 +9,14 @@ const counterValue = document.getElementById('counter-value');
 const incrementBtn = document.getElementById('increment-btn');
 const decrementBtn = document.getElementById('decrement-btn');
 
+
+
+const store = createStore(counterReducer)
+
+const render = function () {
+
+    let currentState = store.getState();
+
+    counterValue.textContent = currentState.count
+
+}
