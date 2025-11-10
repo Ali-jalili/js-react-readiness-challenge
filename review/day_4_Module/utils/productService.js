3ccefa463af5ba83e3e3da48b09018f2b86ece98
Ali Jalili <mrali97jl@gmail.com>
2025-11-10 19:17:40 +0330

    `Added productService module with MASTER_PRODUCTS array and getProducts and setActiveFilter functions`

--- review/day_4_Module/utils/productService.js
+++ review/day_4_Module/utils/productService.js
@@ -1,2 +1,37 @@
 //ماژول مدیریت داده‌ها و منطق فیلتر
 
+const MASTER_PRODUCTS = [
+    { id: 101, name: "لپ‌تاپ گیمینگ X50", category: "electronics" },
+    { id: 102, name: "کتاب معماری تمیز", category: "books" },
+    { id: 103, name: "هدفون بلوتوثی مدل Pro", category: "electronics" },
+    { id: 104, name: "تی‌شرت نخی مردانه", category: "clothing" },
+    { id: 105, name: "کتاب راهنمای جاوااسکریپت", category: "books" },
+    { id: 106, name: "شارژر پرتابل 10000mAh", category: "electronics" },
+    { id: 107, name: "شلوار جین زنانه", category: "clothing" }
+];
+
+
+// activeFilter کارش اینکه هنگام فیلتر مشخص کنه کدوم دسته بندی فعال باشه
+
+let activeFilter = 'all';
+
+
+//گرفتن کل محصولات توسط ماژول دیگه
+
+export const getProducts = function () {
+
+    return MASTER_PRODUCTS
+
+}
+
+
+
+//ماژول‌های دیگر با فراخوانی این تابع، وضعیت activeFilter را تغییر می‌دهند
+
+export const setActiveFilter = function (category) {
+
+    return activeFilter = category
+
+
+
+}
--- review/day_4_Module/utils/viewRenderer.js
+++ review/day_4_Module/utils/viewRenderer.js
@@ -1,2 +1,6 @@
 //ماژول مدیریت DOM
 
+import { getProducts } from "./productService.js";
+
+
+console.log(getProducts());
