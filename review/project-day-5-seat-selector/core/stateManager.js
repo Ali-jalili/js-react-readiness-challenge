90e13c516ccd844e747f87a821d47477c9740be5
Ali Jalili <mrali97jl@gmail.com>
2025-11-10 21:42:06 +0330

    `Added toggleSeatSelection function to manage seat selection and getSeatSummary function to retrieve seat summary`

--- review/project-day-5-seat-selector/core/stateManager.js
+++ review/project-day-5-seat-selector/core/stateManager.js
@@ -9,14 +9,27 @@ let selectedSeatsIds = [];
 
 export const toggleSeatSelection = function (seatId) {
 
+
+    // اگر صندلی قبلاً انتخاب شده:
+
     if (selectedSeatsIds.includes(seatId)) {
 
+        // 1. ایندکس (موقعیت) آن را پیدا کن.
+
         const idItem = selectedSeatsIds.indexOf(seatId);
+
+        // 2. از آن ایندکس شروع کن و یک آیتم را حذف کن (حذف صندلی).
 
         selectedSeatsIds.splice(idItem, 1)
 
     }
+
+    // اگر صندلی انتخاب نشده:
+
     else {
+
+        // آن را اضافه کن.
+
         selectedSeatsIds.push(seatId)
     }
 
@@ -24,4 +37,19 @@ export const toggleSeatSelection = function (seatId) {
 
 }
 
+
+export const getSeatSummary = function () {
+
+    const count = selectedSeatsIds.length;
+
+    const totalPrice = TICKET_PRICE * count;
+
+    return {
+        selectedSeatsIds,
+        count,
+        totalPrice
+
+    }
+
+}
 
