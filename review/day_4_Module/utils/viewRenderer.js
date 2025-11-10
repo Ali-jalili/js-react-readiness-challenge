06d8b4e76b3a6f0965764c1414c467a9ef4f9c41
Ali Jalili <mrali97jl@gmail.com>
2025-11-10 20:03:51 +0330

    `Added new code to viewRenderer.js: product list rendering functionality`

--- review/day_4_Module/utils/viewRenderer.js
+++ review/day_4_Module/utils/viewRenderer.js
@@ -1,2 +1,30 @@
 //ماژول مدیریت DOM
 
+
+
+
+
+const productListContainer = document.getElementById('product-list-container')
+
+
+export const renderProducts = function (productsList) {
+
+    productListContainer.innerHTML = '';
+
+    productsList.forEach(item => {
+
+        const productElement = document.createElement('div');
+
+        productElement.textContent = `${item.name}+${item.category}`
+
+        productListContainer.append(productElement)
+
+
+
+
+    })
+
+
+
+}
+
