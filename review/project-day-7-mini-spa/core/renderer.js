d39d077a14b831a6d20f7904315b6d2fd0c5160e
Ali Jalili <mrali97jl@gmail.com>
2025-11-14 15:37:47 +0330

    `Added renderPosts function and updated DOM queries to use const`

--- review/project-day-7-mini-spa/core/renderer.js
+++ review/project-day-7-mini-spa/core/renderer.js
@@ -3,7 +3,25 @@
 
 // 1. تعریف ثابت‌های DOM
 // المانی که محتوای داینامیک (لودینگ، خطا، داده) در آن تزریق می‌شود.
-const $contentArea = document.querySelector('#content-area');
+const contentArea = document.querySelector('#content-area');
 
 // کانتینر تب‌ها برای مدیریت کلاس 'active'.
-const $tabContainer = document.querySelector('#tab-container');
+const tabContainer = document.querySelector('#tab-container');
+
+
+
+export const renderPosts = (postsArray) => {
+
+    const postItemsHTML = postsArray.map(post => {
+
+        return `
+            <li>
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+            </li>
+        `
+    }).join('')
+
+    return `<div class="posts-list-container"><ul>${postItemsHTML}</ul></div>`;
+
+} 
